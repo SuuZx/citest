00e7096e9b4a65ef184ae44520d340109aa0b996
szx <1621485061@qq.com>
2025-03-25 18:13:42 +0800

    init

--- src/main/java/FloorSquareRoot.java
+++ src/main/java/FloorSquareRoot.java
@@ -1,5 +1,3 @@
-package Samples;
-
 public class FloorSquareRoot {
     public static int floorSqrt(int x) {
         if (x == 0 || x == 1) return x;
--- src/test/java/Test001.java
+++ src/test/java/Test001.java
@@ -1,5 +1,3 @@
-package testcases;
-
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
